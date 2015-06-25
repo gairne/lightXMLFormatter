48931b13a6196ccad94c42d88ca04d6822a9f07e
Matthew Mole 46059196 <Matthew.Mole@kent.pnn.police.uk>
2015-06-25 15:42:26 +0100

    Do not print output to stdout if output filename not specified. Marginally more intelligent backing up implemented

--- src/uk/co/gairne/lxmlf/Main.java
+++ src/uk/co/gairne/lxmlf/Main.java
@@ -15,18 +15,23 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, ParserException, IOException {
 		if (args.length >= 1) {
-			System.out.println(args[0]);
+			System.out.println("Processing " + args[0]);
 			StaxParser s = new StaxParser();
 			String output = s.parseFile(args[0]).toString();
 			if (args.length >= 2) {
-				System.out.println(args[1]);
+				System.out.println("Saving output to " + args[1]);
 				BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1])));
 				out.write(output);
 				out.close();
 			}
 			else {
-				Files.move(new File(args[0]).toPath(), new File(args[0]+".bak").toPath());
-				System.out.println(output);
+				int i = 0;
+				while (!new File(args[0] + ".bak." + i).exists()) {
+					i++;
+				}
+				System.out.println("Backing up " + args[0] + " to " + args[0] + ".bak." + i);
+				Files.move(new File(args[0]).toPath(), new File(args[0]+".bak."+i).toPath());
+				System.out.println("Saving output to " + args[0]);
 				BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[0])));
 				out.write(output);
 				out.close();
